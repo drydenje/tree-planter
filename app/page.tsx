fa6b2722c27e0b5f9b1aeea458f5957908876dbf
Jeremy Dryden <jeremy.dryden@gmail.com>
2023-09-24 22:51:42 -0400

    added a simple flow, starting to use Dagre to apply a vertical layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,57 @@
-// import { Button } from "@/components/ui";
-import { Button } from '@/base/components/ui/button';
+'use client';
+// import { useMemo } from 'react';
+import ReactFlow, { Controls, Background } from 'reactflow';
+import Dagre from 'dagre';
+import 'reactflow/dist/style.css';
+// import { Button } from '@/base/components/ui/button';
 // import FetchGraphQL from "@/src/components/"
 
-const test = process.env.SPACE_ID;
-console.log('test:', test);
+const edges = [
+  {
+    id: '1-2',
+    source: '1',
+    target: '2',
+    label: 'to the',
+    type: 'step',
+  },
+  {
+    id: '1-3',
+    source: '1',
+    target: '3',
+    label: 'to the',
+    type: 'step',
+  },
+];
+
+const nodes = [
+  {
+    id: '1',
+    position: { x: 0, y: 0 },
+    data: { label: 'Hello' },
+    type: 'input',
+  },
+  {
+    id: '2',
+    position: { x: 100, y: 100 },
+    data: { label: 'World' },
+  },
+  {
+    id: '3',
+    position: { x: 200, y: 200 },
+    data: { label: 'Heeyyyyy' },
+  },
+];
+
+// const g = new Dagre.graphlib.Graph().setDefault
 
 export default function Home() {
   return (
-    <main>
-      <Button>Click me, please</Button>
-      <div>test in</div>
-      <div>try without ui library</div>
-    </main>
+    <div style={{ height: '80vh', width: '80vw', border: '1px solid black' }}>
+      <h1>test</h1>
+      <ReactFlow nodes={nodes} edges={edges}>
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
   );
 }
